2b39d6b75267eb14ed0a940fec865d00dbbcb749
Isaac <Isaacsunoo@gmail.com>
2018-08-31 09:11:16 -0600

    Controller, jsp

--- MVCTrailMixer/src/main/java/com/skilldistilery/mvctrailmixer/controllers/TrailMixerController.java
+++ MVCTrailMixer/src/main/java/com/skilldistilery/mvctrailmixer/controllers/TrailMixerController.java
@@ -1,5 +1,11 @@
 package com.skilldistilery.mvctrailmixer.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class TrailMixerController {
+	
+	@Autowired 
+	private
 
 }
