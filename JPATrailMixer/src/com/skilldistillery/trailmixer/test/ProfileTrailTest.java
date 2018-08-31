bc886efad1f8db0f81a8eb25e39417d2f5981ae3
Owen Harris <owenharris21@icloud.com>
2018-08-31 15:52:24 -0600

    junit tests passed

--- JPATrailMixer/src/com/skilldistillery/trailmixer/test/ProfileTrailTest.java
+++ JPATrailMixer/src/com/skilldistillery/trailmixer/test/ProfileTrailTest.java
@@ -11,14 +11,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.skilldistillery.trailmixer.entities.Profile;
-import com.skilldistillery.trailmixer.entities.ProfileTrail;
 import com.skilldistillery.trailmixer.entities.Trail;
 
 class ProfileTrailTest {
 	private EntityManagerFactory emf; 
 	private EntityManager em; 
 	private Trail tr; 
-	private ProfileTrail pt; 
 	private Profile prof; 
 
 	@BeforeEach
@@ -37,6 +35,12 @@ class ProfileTrailTest {
 	@Test
 	public void test_profiletrail_to_profile_on_rating() {
 		assertEquals(4, prof.getPts().get(0).getRating()); 
+	}
+	
+	@Test
+	public void test_profiletrail_to_trail_on_rating() {
+		tr = em.find(Trail.class, 1); 
+		assertEquals(4, tr.getPts().get(0).getRating()); 
 	}
 
 }
