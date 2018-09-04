45aaea6818d6bde6e4f3dfef8e875c6919ba0aea
liamkimbley <liam.kimbley@gmail.com>
2018-09-04 10:33:59 -0600

    project updated

--- MVCTrailMixer/src/main/java/com/skilldistillery/mvctrailmixer/controllers/MatchesController.java
+++ MVCTrailMixer/src/main/java/com/skilldistillery/mvctrailmixer/controllers/MatchesController.java
@@ -1,14 +1,31 @@
 package com.skilldistillery.mvctrailmixer.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.skilldistillery.mvctrailmixer.data.TrailsDAO;
+import com.skilldistillery.trailmixer.entities.Profile;
+import com.skilldistillery.trailmixer.entities.Trail;
 
 public class MatchesController {
+	
+	@Autowired
+	private TrailsDAO tdao;
 	
 	public static final String USER_IN_SESSION_KEY = "UserInSession";
 
 	@RequestMapping(path="", method=RequestMethod.GET)
-	public String getMatches() {
+	public String getMatchesByDistance(Profile profile) {
+		ModelAndView mv = new ModelAndView();
+		List<Trail> trails = tdao.getListOfTrails();
+		
+		for (Trail trail : trails) {
+			if (trail.getDistance() >= profile.getPreferences().)
+		}
 		
 		return "hikes/matches";
 	}
