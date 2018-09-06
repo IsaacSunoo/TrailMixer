20f9f962a77296b9b62522f12226d15b0da55e97
Isaac <Isaacsunoo@gmail.com>
2018-09-06 09:31:28 -0600

    update' ;

--- MVCTrailMixer/src/main/java/com/skilldistillery/mvctrailmixer/controllers/TrailController.java
+++ MVCTrailMixer/src/main/java/com/skilldistillery/mvctrailmixer/controllers/TrailController.java
@@ -31,10 +31,10 @@ public class TrailController {
 	}
 	
 	@RequestMapping(path="TrailDetails.do", method = RequestMethod.GET)
-	public ModelAndView getDetails(@RequestParam Integer trailId) {
+	public ModelAndView getDetails(int id) {
 		ModelAndView mv = new ModelAndView(); 
-		Trail trail = dao.getTrailDetails(trailId); 
-		Double rating = dao.getTrailRating(trailId);
+		Trail trail = dao.getTrailDetails(id); 
+		Double rating = dao.getTrailRating(id);
 		mv.addObject("rating", rating); 
 		mv.addObject("trail", trail); 
 		mv.setViewName("trails/TrailDetails");
@@ -139,6 +139,7 @@ public class TrailController {
 	
 	private Trail getCurrentTrailFromSession(HttpSession session) {
 	    Trail current = (Trail) session.getAttribute("trail");
+	    
 	    return current;
 	  }
 
